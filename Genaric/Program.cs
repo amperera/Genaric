1e6790119e9ee1fb697f2905b070883da7b1034b
Amila <madusanka.lk@gmail.com>
2014-08-23 21:29:02 +0530

    first genaric

---
+++ Genaric/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genaric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dd = new Settingdata<int,string> { MyProperty = 1, data = 5, Data2 ="this is string"};
+
+            var pp = new print();
+            pp.Printingdata(dd);
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Settingdata<T,K>
+    {
+        public int MyProperty { get; set; }
+        public T data { get; set; }
+
+        public K Data2 { get; set; }
+    }
+    public class print
+    {
+        public void Printingdata<T, K>(Settingdata<T, K> data)
+        {
+
+            Console.WriteLine(data.MyProperty);
+            Console.WriteLine(data.data);
+            Console.WriteLine(data.Data2);
+        }
+    }
+
+
+  
+}
